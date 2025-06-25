db2fcfee61c8725aeb5ab4268ba9b3dd66948617
tsabitgholib <tsabitgholib@gmail.com>
2025-06-25 17:11:17 +0700

    tes view

--- resources/views/buku-besar.blade.php
+++ resources/views/buku-besar.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 <h2>Buku Besar</h2>
-<form method="GET" action="http://localhost/AkuntansiKeuangan/backend_keuangan/buku-besar" class="mb-3">
+<form method="GET" action="http://103.23.103.43/AkuntansiKeuangan/backend_keuangan/buku-besar" class="mb-3">
     <div class="row g-2">
         <div class="col"><input type="number" class="form-control" name="akun_id" placeholder="Akun ID" value="{{ $akunId }}" required></div>
         <div class="col"><input type="number" class="form-control" name="periode_id" placeholder="Periode ID" value="{{ $periodeId }}" required></div>
